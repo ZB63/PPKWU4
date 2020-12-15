647b920b8ade2065c383d83ea52aacefef63e709
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-12-15 15:09:06 +0100

    Parse and convert to json single elements

--- src/main/java/com/ppkwu/ppkwu4/controller/VCardController.java
+++ src/main/java/com/ppkwu/ppkwu4/controller/VCardController.java
@@ -1,5 +1,7 @@
 package com.ppkwu.ppkwu4.controller;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class VCardController {
@@ -20,7 +23,19 @@ public class VCardController {
                 .connect(url)
                 .get();
 
-        Elements table = doc.select("script");
+        Elements elements = doc.select("script");
+
+
+
+        for(int i=0;i<elements.size() - 1;i++) {
+
+            if(elements.get(i).attr("type").equals("application/ld+json")) {
+                String json = elements.get(i).data();
+                //System.out.println(json);
+                JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+                //System.out.println(jsonObject.get("name"));
+            }
+        }
 
         return url;
     }
