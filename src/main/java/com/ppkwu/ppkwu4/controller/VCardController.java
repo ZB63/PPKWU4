0842039765dc3e4e91729a9174f0e3675d7329f5
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-12-15 14:51:39 +0100

    Get website data

--- src/main/java/com/ppkwu/ppkwu4/controller/VCardController.java
+++ src/main/java/com/ppkwu/ppkwu4/controller/VCardController.java
@@ -1,17 +1,26 @@
 package com.ppkwu.ppkwu4.controller;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class VCardController {
 
     @GetMapping("test/{name}")
-        public String test(@PathVariable String name) {
+        public String test(@PathVariable String name) throws IOException {
         String url = "https://panoramafirm.pl/szukaj?k=" + name;
 
+        Document doc = Jsoup
+                .connect(url)
+                .get();
 
+        Elements table = doc.select("script");
 
         return url;
     }
