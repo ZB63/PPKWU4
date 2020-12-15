2b96dd8cc3fd88def8b2f15319f894b6503a9d4a
Sebastian Paweloszek <spaweloszek63@gmail.com>
2020-12-15 14:38:03 +0100

    Create test function

--- src/main/java/com/ppkwu/ppkwu4/controller/VCardController.java
+++ src/main/java/com/ppkwu/ppkwu4/controller/VCardController.java
@@ -1,7 +1,17 @@
 package com.ppkwu.ppkwu4.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VCardController {
+
+    @GetMapping("test")
+    public String test(@RequestParam(value = "name") String name) {
+        String url = "https://panoramafirm.pl/szukaj?k=" + name;
+
+        return url;
+    }
+
 }
