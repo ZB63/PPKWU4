e13cb9bff85fca60a8753c961e556dcec6f99ecb
Sebastian Paweloszek <spaweloszek63@gmail.com>
2021-01-12 14:39:57 +0100

    Build a proper vcard file

--- src/main/java/com/ppkwu/ppkwu4/controller/VCardController.java
+++ src/main/java/com/ppkwu/ppkwu4/controller/VCardController.java
@@ -3,14 +3,19 @@ package com.ppkwu.ppkwu4.controller;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.ppkwu.ppkwu4.dto.VCardDTO;
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import ezvcard.VCardVersion;
+import ezvcard.parameter.EmailType;
+import ezvcard.parameter.TelephoneType;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import sun.tools.jconsole.JConsole;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -62,6 +67,11 @@ public class VCardController {
                 JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
                 if(jsonObject.has("name") && jsonObject.get("name").getAsString().equals(name)) {
                     // here build and send vcard
+                    VCard vCard = jsonToVCard(jsonObject);
+
+                    File file = new File("VCard - " + jsonObject.get("name").getAsString() + ".vcf");
+                    Ezvcard.write(vCard).version(VCardVersion.V3_0).go(file);
+
                     System.out.println("Found " + name);
                 }
             }
@@ -85,6 +95,25 @@ public class VCardController {
             vCardDTO.setUrl(jsonObject.get("sameAs").getAsString());
         }
         return vCardDTO;
+    }
+
+    private VCard jsonToVCard(JsonObject jsonObject) {
+        VCard vcard = new VCard();
+
+        if(jsonObject.has("name")) {
+            vcard.setFormattedName(jsonObject.get("name").getAsString());
+        }
+        if(jsonObject.has("email")) {
+            vcard.addEmail(jsonObject.get("email").getAsString(), EmailType.WORK);
+        }
+        if(jsonObject.has("telephone")) {
+            vcard.addTelephoneNumber(jsonObject.get("telephone").getAsString(), TelephoneType.WORK);
+        }
+        if(jsonObject.has("sameAs")) {
+            vcard.addUrl(jsonObject.get("sameAs").getAsString());
+        }
+
+        return vcard;
     }
 
 }
